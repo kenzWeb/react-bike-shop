81e498b10aa1f7bc4c4eae409548f161052a6532
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2022-11-22 20:11:16 +0300

    Сверстал форму отправки инофрмации на странице Contato

--- src/Components/Pages/ContatoP.js
+++ src/Components/Pages/ContatoP.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from '../Common/Header'
+import Contato from "../Contato/Contato";
 
 function ContatoP() {
 	return (
 		<>
 			<Header/>
+
+			<main>
+				<Contato/>
+			</main>
 		</>
 	)
 }
